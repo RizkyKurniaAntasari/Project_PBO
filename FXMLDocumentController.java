1ab855b38400c022da4b48f36df471ca2f601ce5
KurAnt1313 <148110487+RizkyKurniaAntasari@users.noreply.github.com>
2024-12-07 22:13:06 +0700

    Update FXMLDocumentController.java
    nambahin class corner

--- FXMLDocumentController.java
+++ FXMLDocumentController.java
@@ -137,7 +137,35 @@ public class FXMLDocumentController implements Initializable {
         left, right, up, down;
     }
 
+    public static class Corner {
+        int x, y;
 
+
+        public Corner(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+
+        public int getX() {
+            return x;
+        }
+
+
+        public void setX(int x) {
+            this.x = x;
+        }
+
+
+        public int getY() {
+            return y;
+        }
+
+
+        public void setY(int y) {
+            this.y = y;
+        }
+    }
 
     }       
 }
