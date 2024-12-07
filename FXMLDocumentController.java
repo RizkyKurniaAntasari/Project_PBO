2e2a995944cf87ac9c4dac257775552b28e4206d
KurAnt1313 <148110487+RizkyKurniaAntasari@users.noreply.github.com>
2024-12-07 21:18:01 +0700

    Update FXMLDocumentController.java
    import baru

--- FXMLDocumentController.java
+++ FXMLDocumentController.java
@@ -10,6 +10,15 @@ import javafx.scene.input.KeyEvent;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import javafx.scene.control.Label;
 /**
  *
  * @author Rizky Kurnia Antasari
