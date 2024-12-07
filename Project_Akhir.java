e1df75a9c2f34b2b4b15dd11e0e394e8b7e1b1ae
KurAnt1313 <148110487+RizkyKurniaAntasari@users.noreply.github.com>
2024-12-07 10:30:54 +0700

    Update Project_Akhir.java
    sedikit melakukan perubahan pada nama, dan import, serta menambahkan detail kecil seperti title

--- Project_Akhir.java
+++ Project_Akhir.java
@@ -1,36 +1,24 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML.java to edit this template
- */
-package awalan;
+package project_akhir;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
-/**
- *
- * @author Rizky Kurnia Antasari
- */
-public class Awalan extends Application {
-    
+/*
+* @Author Rizky Kurnia Antasari
+*/
+public class Project_Akhir extends Application {
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
         Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.show();
+        primaryStage.setTitle("Snake Game");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         launch(args);
     }
-    
 }
