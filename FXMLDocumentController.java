f97325d295669f8bbbe6cb6cf41f09ede0317b56
KurAnt1313 <148110487+RizkyKurniaAntasari@users.noreply.github.com>
2024-12-07 21:07:48 +0700

    Update FXMLDocumentController.java
    mengganti package dan menambahkan import baru

--- FXMLDocumentController.java
+++ FXMLDocumentController.java
@@ -1,15 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML2.java to edit this template
- */
-package awalan;
+package project_akhir;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.event.ActionEvent;
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 
 /**
  *
